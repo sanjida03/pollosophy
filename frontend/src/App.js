0d3fb3549ab3ab83bad310bb91474478ab2c3e4d
DevouredByOneMan <jonathanphkim2003@gmail.com>
2024-11-04 11:31:22 -0500

    added Sign Up feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ActivePolls from './pages/ActivePolls';
 import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/active-polls" element={<ActivePolls />} />
+        <Route path="/sign-up" element ={<SignUp />} />
         <Route path="/login" element={<LogIn/>}/>
       </Routes>
     </Router>
