ee6ed30f4bbca39c72bdab127c9218d0b334c0f2
sanjida03 <sshehri1@umbc.edu>
2024-11-06 21:40:03 -0500

    added about page

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/header';
+
+function about(){
+
+}
+
+export default about;
